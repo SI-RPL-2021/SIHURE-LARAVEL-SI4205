976a4c3f7700e3db8c380c2f85bbcb8d7758355e
AnastassyaG <anastassya@student.telkomuniversity.ac.id>
2021-04-09 15:55:32 +0700

    Update

--- Laravel1/resources/views/welcome.blade.php
+++ Laravel1/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 @section('content_header')
 <br>
-    <h1 style='text-align:center; color:#6F7D87; font-family:Open Sans,Arial,sans-serif; font-size:50px; font-weight: 1000;'>WELCOME TO SIHURE</h1>
+    <h1 style='text-align:center; color:#6F7D87; font-family:Open Sans,Arial,sans-serif; font-size:50px; font-weight: 500;'>WELCOME TO SIHURE</h1>
     <br>
     <br>
     <img src="vendor/adminlte/dist/img/Logo SIHURE white.png" style='margin-left:auto; margin-right:auto; display:block; width:40%;'>
