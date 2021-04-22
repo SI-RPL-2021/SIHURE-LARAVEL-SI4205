f96b9459f90fb896c032e5811e4ec635ca88c173
adlighozian <adlighozian1@gmail.com>
2021-04-22 13:18:10 +0700

    controller untuk fitur jatah cuti, absen, status absen ref #4 #3 #2

--- Laravel1/routes/web.php
+++ Laravel1/routes/web.php
@@ -17,6 +17,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController::class, 'index']);
+
+
+
+// Route::get('/karyawan/absensi',  'App\Http\Controllers\karyawanController@absensi')->name('absensi');
+// Route::get('/karyawan/cuti', 'App\Http\Controllers\karyawanController@cuti')->name('cuti');
+// Route::get('/karyawan/lembur', [karyawanController::class, 'lembur']);
+// Route::get('/karyawan/karyawan', [karyawanController::class, 'karyawan']);
+// Route::get('/karyawan/gaji', [karyawanController::class, 'gaji']);
+// Route::post('/karyawan/todo', [karyawanController::class, 'todo']);
+// Route::get('/karyawan/absenpulang', [karyawanController::class, 'absenPulang']);
+// Route::post('/karyawan/tabel', [karyawanController::class, 'buattabel']);
+
+// Route::get('/hr/absensi', [hrController::class, 'absensi']);
+// Route::get('/hr/cuti', [hrController::class, 'cuti'])->name('hrcuti');
+// Route::get('/hr/karyawan', [hrController::class, 'karyawan']);
+// Route::get('/hr/lembur', [hrController::class, 'lembur']);
+// Route::get('/hr/penggajian', [hrController::class, 'penggajian']);
+// Route::post('/hr/approvecuti', [hrController::class, 'approve']);
